4b8c09c615b83fb1e01502612a4a17abfe7e4364
aurelien.etienne <aurelien.etienne@epita.fr>
2025-07-03 12:37:48 +0200

    6 paquets de cartes

--- script.js
+++ script.js
@@ -67,15 +67,22 @@ function submitPlayers(event) {
 
 function createDeck() {
   deck = [];
-  for (let suit of suits) {
-    for (let rank of ranks) {
-      deck.push({ rank, suit });
+  const numDecks = 6; // Play with 6 decks
+
+  for (let i = 0; i < numDecks; i++) {
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        deck.push({ rank, suit });
+      }
     }
   }
+  
+  // Shuffle the combined deck
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
   }
+  console.log(`Deck created with ${deck.length} cards (simulating ${numDecks} decks).`);
 }
 
 function getCardValue(card) {
